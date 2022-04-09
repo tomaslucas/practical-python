d5a9ccfa8984def3156ba45264a8b641d33560b1
Tomás Lucas <tomaslucas@gmail.com>
2022-04-09 12:14:58 +0200

    Aplico ejercicio 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
 def portfolio_cost(filename):
     total_price = 0
     with open(filename, 'rt') as f:
         try:
-            header = next(f)
-            for line in f:
-                l = line.strip().split(',')
-                total_price += int(l[1]) * float(l[2])
+            rows = csv.reader(f)
+            header = next(rows)
+            for row in rows:
+                total_price += int(row[1]) * float(row[2])
         except Exception as e:
             print('Error encontrado en el fichero: ', e)
 
     return total_price
 
 if __name__ == '__main__':
-    cost = portfolio_cost('Data/portfolio.csv')
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    
+    cost = portfolio_cost(filename)
     print('Total cost:', cost)
