0410ca7e50655fcce0b7912b8e45a434bb385539
Tomás Lucas <tomaslucas@gmail.com>
2022-04-11 18:39:14 +0200

    Exercise 3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,26 +4,17 @@
 import csv
 import sys
 
+from report import read_portfolio
+
 def portfolio_cost(filename):
-    total_cost = 0
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += nshares * price
-            except ValueError:
-                print(f'Row {rowno}: Bad row: {row}')    
-    return total_cost
+    portfolio = read_portfolio(filename)
+    return sum([record['shares'] * record['price'] for record in portfolio])
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         filename = sys.argv[1]
     else:
         filename = 'Data/portfolio.csv'
-    
+        
     cost = portfolio_cost(filename)
     print('Total cost:', cost)
