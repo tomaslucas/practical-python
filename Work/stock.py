5ae8f9ca143635dae364e4b3e7d4cadf8248ef1a
Tomás Lucas <tomaslucas@gmail.com>
2022-04-14 12:15:41 +0200

    Exercise 5.7

--- Work/stock.py
+++ Work/stock.py
@@ -14,6 +14,17 @@ class Stock():
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
 
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
+
+
 class NewStock(Stock):
     def yow(self):
         print('Yow!')
