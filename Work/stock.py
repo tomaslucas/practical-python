b208588540ac6b58394ace0348585344542e674b
Tomás Lucas <tomaslucas@gmail.com>
2022-04-16 20:02:18 +0200

    Exercise 7.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,10 @@
+from typedproperty import *
+
 class Stock():
-    __slots__ = ('name', '_shares', 'price')
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
     def __init__(self, name, shares, price) -> None:
         self.name = name
         self.shares = shares
@@ -14,16 +19,6 @@ class Stock():
 
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
-
-    @property
-    def shares(self):
-        return self._shares
-    
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
 
 
 class NewStock(Stock):
